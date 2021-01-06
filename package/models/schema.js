7b475de5182d53a9e06ff52e7bcc398a5903c313
wangrui <wangrui@hupu.com>
2021-01-06 19:27:36 +0800

    feat: 测试看

--- package/models/schema.js
+++ package/models/schema.js
@@ -23,6 +23,7 @@ export default {
   },
 
   changeNameAction: function(state, action, oldState) {
+    console.log('这里看', state, action, oldState )
     const keys = action.prefix;
     const name = action.name;
     const value = action.value;
